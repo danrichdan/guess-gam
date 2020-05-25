86eca3962b68c3dc0130a602fa27416f901dc15c
Dan Riches <driches@ymail.com>
2020-05-25 12:29:03 -0700

    Win and lose functions created

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const guessBtn = document.getElementById("guess-button");
 const guessInput = document.getElementById("guess");
 const numberToGuess = getRandomNumber();
 const msg = document.querySelector(".help");
-let guessesLeft;
-console.log(`The number to guess is${numberToGuess}`);
+let guessesLeft = 3;
+console.log(`The number to guess is ${numberToGuess}`);
 
 // Event Listener
 guessBtn.addEventListener("click", evaluateGuess);
@@ -17,23 +17,33 @@ function getRandomNumber() {
 function evaluateGuess() {
   let guess = guessInput.value;
   guess = Number(guess);
-
+  guessesLeft -= 1;
   if (guess === numberToGuess) {
-    gameWin();
+    gameWin(guess, guessesLeft);
+  } else if (guess !== numberToGuess && guessesLeft === 0) {
+    gameLose(guess, guessesLeft);
   } else {
-    console.log(`You guessed ${guess}, and you lose`);
+    console.log(
+      `You guessed ${guess}, but that is wrong, you have ${guessesLeft} guesses left.`
+    );
     guessInput.value = "";
   }
 }
 
-function gameWin() {
+function gameWin(guess, guessesLeft) {
   guessInput.setAttribute("disabled", "true");
   guessInput.classList.add("is-success");
   msg.classList.add("is-success");
+  msg.textContent = `${guess} is correct. You win!`;
   msg.style.display = "block";
   guessBtn.textContent = "Play Again";
 }
 
-function wrongGuess() {
-  console.log("You Lose");
+function gameLose(guess, guessesLeft) {
+  guessInput.setAttribute("disabled", "true");
+  guessInput.classList.add("is-danger");
+  msg.classList.add("is-danger");
+  msg.textContent = `${guess} is wrong, ${guessesLeft} guesses left, you lose.`;
+  msg.style.display = "block";
+  guessBtn.textContent = "Play Again";
 }
